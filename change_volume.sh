467010f2f41f9340cd2f542dc6aa8d9745f827f6
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2021-12-26 21:52:42 +0200

    script updated

--- change_volume.sh
+++ change_volume.sh
@@ -12,4 +12,15 @@ readonly DIRECTION=$3
 
 readonly VOLUME=$(ddcutil getvcp ${VOLUME_CAPABILITY} | grep -oP '(?<=Volume level: )[0-9]+')
 
-echo ${VOLUME}
+echo Current volume is ${VOLUME}
+
+if [ ${DIRECTION} -eq 'up' ]; then
+    let "VOLUME+=${VOLUME_STEP}"
+elif [ ${DIRECTION} -eq 'down' ]; then
+    let "VOLUME-=${VOLUME_STEP}"
+else
+    echo Unknown volume change direction, please choose up or down.
+    exit 2
+fi
+
+echo Current volume is ${VOLUME}

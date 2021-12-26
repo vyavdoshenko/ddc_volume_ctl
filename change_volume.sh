150fe7b2a362a68fdde6b7748d0e0df71358d725
Vladimir Yavdoshenko <v.yavdoshenko@gmail.com>
2021-12-26 22:05:33 +0200

    script fixed

--- change_volume.sh
+++ change_volume.sh
@@ -6,21 +6,30 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
-readonly local VOLUME_CAPABILITY=$1
-readonly local VOLUME_STEP=$2
-readonly local DIRECTION=$3
+readonly VOLUME_CAPABILITY=$1
+readonly VOLUME_STEP=$2
+readonly DIRECTION=$3
 
-local VOLUME=$(ddcutil getvcp ${VOLUME_CAPABILITY} | grep -oP '(?<=Volume level: )[0-9]+')
+VOLUME=$(ddcutil getvcp ${VOLUME_CAPABILITY} | grep -oP '(?<=Volume level: )[0-9]+')
 
 echo Current volume is ${VOLUME}
 
 if [ "${DIRECTION}" = "up" ]; then
     let "VOLUME+=${VOLUME_STEP}"
+    if [${VOLUME} -ge 101]; then
+        VOLUME=100
+    fi
 elif [ "${DIRECTION}" = "down" ]; then
-    let "VOLUME-=${VOLUME_STEP}"
+    if [${VOLUME} -ge 10]; then
+        let "VOLUME-=${VOLUME_STEP}"
+    else
+        VOLUME=0
+    fi
 else
     echo Unknown volume change direction, please choose up or down.
     exit 2
 fi
 
-echo Current volume is ${VOLUME}
+echo Changed volume is ${VOLUME}
+
+ddcutil setvcp ${VOLUME_CAPABILITY} ${VOLUME}
